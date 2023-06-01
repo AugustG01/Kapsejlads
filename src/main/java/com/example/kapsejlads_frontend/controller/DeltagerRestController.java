ad6b25d73ef668a6176ced9630df9717dee89d47
AugustG01 <augu8204@stud.kea.dk>
2023-06-01 18:20:16 +0200

    Alle dropdowns virker

--- src/main/java/com/example/kapsejlads_frontend/controller/DeltagerRestController.java
+++ src/main/java/com/example/kapsejlads_frontend/controller/DeltagerRestController.java
@@ -1,11 +1,31 @@
 package com.example.kapsejlads_frontend.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.kapsejlads_frontend.model.Deltager;
+import com.example.kapsejlads_frontend.model.Kapsejlads;
+import com.example.kapsejlads_frontend.repository.DeltagerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin
 public class DeltagerRestController {
 
+    @Autowired
+    DeltagerRepository deltagerRepository;
 
+    @GetMapping("/alleDeltagelser")
+    public List<Deltager> deltagerList() {
+        return deltagerRepository.findAll();
+    }
+
+    @PostMapping("/createDeltagelser")
+    public ResponseEntity<Deltager> addDeltagerlser(@RequestBody Deltager deltager) {
+        System.out.println(deltager);
+        Deltager nyDeltagelse = deltagerRepository.save(deltager);
+        return new ResponseEntity<>(nyDeltagelse, HttpStatus.CREATED);
+    }
 }
--- src/main/resources/js/dropdown.js
+++ src/main/resources/js/dropdown.js
@@ -1,8 +1,10 @@
 console.log("jeg er i dropdown")
 const urlKapsejlads = "http://localhost:8080/alleKapsejlads"
+const urlBåde = "http://localhost:8080/allBoats"
 
 const pbFillDropDown = document.getElementById("pbFillDropDown")
 const ddKapsejlads = document.getElementById("ddKapsejlads")
+const ddBåde = document.getElementById("ddBåde")
 
 function fetchKapsejlads(url){
     return fetch(url).then((response) => response.json())
@@ -11,9 +13,23 @@ function fetchKapsejlads(url){
 function showKapsejlads(kapsejlads){
     console.log(kapsejlads)
 }
+function showBåde(både){
+    console.log(både)
+}
+
+function actionShowBåde(){
+    showAlleBåde()
+}
 
 function actionShowKapsejlads(){
     showAlleKapsejlads()
+}
+
+async function showAlleBåde(){
+    const bådeList = await fetchKapsejlads(urlBåde);
+    console.log(bådeList)
+    bådeList.forEach(showBåde)
+    fillDropDownBåde(bådeList)
 }
 
 async function showAlleKapsejlads(){
@@ -30,9 +46,25 @@ function addKapsejladsToDropDown(item){
     ddKapsejlads.append(el)
 }
 
-function fillDropDown(kapsejlList) {
+function addBådeToDropDown(item){
+    console.log(item)
+    const el = document.createElement("option")
+    el.textContent = item.bådId;
+    ddBåde.append(el)
+}
+
+function fillDropDown(kapsejlList){
     kapsejlList.forEach(addKapsejladsToDropDown)
 }
 
+function fillDropDownBåde(bådeList){
+    bådeList.forEach(addBådeToDropDown)
+}
+
 pbFillDropDown.addEventListener("click", actionShowKapsejlads)
+pbFillDropDown.addEventListener("click", actionShowBåde)
 ddKapsejlads.addEventListener("change", select)
+
+
+
+
