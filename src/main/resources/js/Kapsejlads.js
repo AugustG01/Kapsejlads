805918f0631d0d4bfbc9899523c694eb8452f566
AugustG01 <augu8204@stud.kea.dk>
2023-06-01 17:33:11 +0200

    Rediger virker også nu

--- src/main/resources/js/Kapsejlads.js
+++ src/main/resources/js/Kapsejlads.js
@@ -16,3 +16,41 @@ function opretKapsejlads() {
 
     postBåd(urlPostKapsejlads, JSON.stringify(kapsejlads));
 }
+
+
+
+function submitForm(event) {
+    event.preventDefault(); // Prevent form submission
+
+    // Get form data
+    var kapsejladsId = document.getElementById('kapsejladsId').value;
+    var navn = document.getElementById('navn').value;
+
+    // Prepare the data object
+    var data = {
+        kapsejladsId: kapsejladsId,
+        navn: navn
+    };
+
+    // Send the PUT request to update the boat
+    fetch(`http://localhost:8080/kapsejlads/${kapsejladsId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Boat updated successfully:', data);
+            // You can add any further logic or UI updates here
+            location.reload()
+        })
+        .catch(error => {
+            console.error('Error updating boat:', error);
+            // Handle the error gracefully
+        });
+}
+
+// Add event listener to the form
+document.getElementById('editKapsejladsForm').addEventListener('submit', submitForm);
